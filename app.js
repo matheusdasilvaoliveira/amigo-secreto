f5580751043798631bfe9b66482fd6a16ddd4415
Matheus Oliveira <devmatheusoliveira01@gmail.com>
2025-08-16 12:47:34 -0300

    implementa logica da lista de amigos e sorteio

--- app.js
+++ app.js
@@ -1,11 +1,51 @@
-function adicionarAmigo() {
-    checaNomeInserido();
+function limpaCampoNome(tag) {
+    document.querySelector(tag).value = '';
 }
 
 function checaNomeInserido() {
-    const nomeAmigo = document.getElementById('amigo').value;
+    const tagCampoInput = '#amigo';
+    const nomeAmigo = document.querySelector(tagCampoInput).value;
 
     if (nomeAmigo == '') {
-        alert('Nome inválido');
+        alert('Por favor, insira um nome.');
+    }
+    else if (amigos.includes(nomeAmigo)) {
+        alert('Nome repetido, insira um nome diferente.');
+    }
+    else {
+        amigos.push(nomeAmigo);
+    }
+
+    limpaCampoNome(tagCampoInput);
+    console.log(amigos);
+}
+
+function criaAmigoNaLista() {
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        console.log('Estou aqui');
+        const itemAmigo = document.createElement('li');
+        itemAmigo.className = 'item-amigo';
+        itemAmigo.innerHTML = amigos[i];
+        listaAmigos.appendChild(itemAmigo);   
     }
 }
+
+function geraNumeroAleatorio(max) {
+    return Math.floor(Math.random() * max);
+} 
+
+function sortearAmigo() {
+    const numeroAmigoSorteado = geraNumeroAleatorio(amigos.length);
+    const campoResultadoSorteio = document.getElementById('resultado');
+    campoResultadoSorteio.innerHTML = amigos[numeroAmigoSorteado];
+}
+
+function adicionarAmigo() {
+    checaNomeInserido();
+    criaAmigoNaLista();
+}
+
+let amigos = [];
